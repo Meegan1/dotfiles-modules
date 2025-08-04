50e3e00fa3078f410db40573e316121e9eb93ae4
Meegan1 <meegan118@gmail.com>
2025-08-04 10:25:23 +0100

    feat(rust): update Fenix overlay to commit f53ddf7 with new sha256

--- modules/rust/default.nix
+++ modules/rust/default.nix
@@ -5,8 +5,8 @@
       nixpkgs.overlays = [
         (import "${
           fetchTarball {
-            url = "https://github.com/nix-community/fenix/archive/main.tar.gz";
-            sha256 = "0wfa9qlhm7shxdkhxcwy5w8fl10x5s733m3l1h5r6f2kmsyi94fh";
+            url = "https://github.com/nix-community/fenix/archive/f53ddf7518d85d59b58df6e9955b25b0ac25f569.zip";
+            sha256 = "0zd51sqp5kz2kx6cyvfd9by3cwnslc7ny4jfp3ys6bjyy7bjc1ib";
           }
         }/overlay.nix")
       ];
