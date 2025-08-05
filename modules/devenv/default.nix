5e42c74ff7c12e0660834f3a0d4732bab3b5d889
Meegan1 <meegan118@gmail.com>
2025-08-05 10:42:37 +0100

    fix: pin catppuccin process-compose and sbarlua to specific commits

--- modules/devenv/default.nix
+++ modules/devenv/default.nix
@@ -12,7 +12,7 @@
             pkgs.fetchFromGitHub {
               owner = "catppuccin";
               repo = "process-compose";
-              rev = "main"; # Pin to a specific commit for reproducibility
+              rev = "b0c48aa07244a8ed6a7d339a9b9265a3b561464d"; # Pin to a specific commit for reproducibility
               hash = "sha256-uqJR9OPrlbFVnWvI3vR8iZZyPSD3heI3Eky4aFdT0Qo=";
             }
             + "/themes/catppuccin-mocha.yaml";
--- modules/sketchybar/packages/sbarlua.nix
+++ modules/sketchybar/packages/sbarlua.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "FelixKratz";
     repo = "SbarLua";
-    rev = "main";
+    rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
     sha256 = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
   };
 
