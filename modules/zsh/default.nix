28048bf3c507cc65ed69dc0d160d536c1f3816ca
Meegan1 <jake@meegan.me>
2025-11-13 01:12:00 +0000

    feat: bind Ctrl+Y to accept zsh-autosuggestions and add history search keybindings for Ctrl+P/Ctrl+N

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -32,6 +32,16 @@
             "belak/zsh-utils path:completion"
           ];
         };
+
+        # bind Ctrl+Y to accept the autosuggestion
+        initContent = ''
+          # accept the current zsh-autosuggestions suggestion with Ctrl+Y
+          bindkey '^Y' autosuggest-accept
+
+          # jump to previous history entry that begins with the current buffer (Ctrl+P)
+          bindkey '^P' history-beginning-search-backward
+          bindkey '^N' history-beginning-search-forward
+        '';
       };
     };
 }
