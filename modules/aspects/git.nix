60a6135adbfa2f4d7a2277af87feda48c303afd8
Meegan1 <jake@meegan.me>
2026-09-16 15:10:57 +0100

    feat: add git-wt to git aspect

--- modules/aspects/git.nix
+++ modules/aspects/git.nix
@@ -1,9 +1,19 @@
 {
   dotfiles-modules.git = {
-    homeManager = {
+    homeManager = { pkgs, lib, ... }: {
       programs.git = {
         enable = true;
       };
+
+      home.packages = with pkgs; [
+        git-wt
+      ];
+
+      programs.zsh.initContent = (
+        lib.mkAfter ''
+          eval "$(git wt --init zsh)"
+        ''
+      );
     };
   };
 }
